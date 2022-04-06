fdb5225eaf370b3310924ad241ec82bc30664c16
Gabriel Cirstea <gabriel_cirstea@protonmail.com>
2022-04-06 23:46:10 +0300

    Complete first practice

--- lab3/inotify/in.c
+++ lab3/inotify/in.c
@@ -77,31 +77,33 @@ int access_file(struct static_vec *fls, const char* name)
   return 0;
 }
 
+char* get_wd_path(struct static_vec*, int);
+
 /* Count the access times of a file in fls and print the event from inotify
  * @event - the inotify event to read
  * @fls - the simple static allocated vector with the file list */
-void count_file(struct inotify_event *event, struct static_vec *fls)
+void count_file(char* file, struct inotify_event *event, struct static_vec *fls)
 {
   if ( event->mask & IN_CREATE ) {
     if ( event->mask & IN_ISDIR ) {
-      printf( "New directory %s created.\n", event->name );
+      printf( "New directory %s created.\n", file );
     }
     else {
-      printf( "New file %s created.\n", event->name );
+      printf( "New file %s created.\n", file );
     }
   }
   else if ( event->mask & IN_DELETE ) {
     if ( event->mask & IN_ISDIR ) {
-      printf( "Directory %s deleted.\n", event->name );
+      printf( "Directory %s deleted.\n", file );
     }
     else {
-      printf( "File %s deleted.\n", event->name );
+      printf( "File %s deleted.\n", file );
     }
   }
   else if (event->mask & IN_ACCESS ) {
-    access_file(fls, event->name);
-    int nr = get_acces_no(fls, event->name);
-    printf( "File %s accessed %d times.\n", event->name, nr );
+    access_file(fls, file);
+    int nr = get_acces_no(fls, file);
+    printf( "File %s accessed %d times.\n", file, nr );
   }
 }
 
@@ -120,13 +122,108 @@ int is_dir(const char *name)
   return 0;
 }
 
+/* Add a new watcher for the fd notifier
+ * @fd - file descritpor for inotify
+ * @wfds - static vector for watchers fds
+ * @path - path to file
+ * @reutrn - only 0 on succes */
+int add_new_watcher(int fd, struct static_vec *wfds, const char *path)
+{
+	if(!path || !wfds) {
+		return -1;
+	}
+	int wd = inotify_add_watch( fd, path,
+			IN_ACCESS | IN_CREATE | IN_DELETE);
+	int n = wfds->len++;
+	wfds->files[n].count = wd;
+	strncpy(wfds->files[n].name, path, PATH_MAX);
+	fprintf(stderr, "New wathcer for %s, wd: %d\n", path, wd);
+	return 0;
+}
+
+/* Remove a watcher from inotify
+ * @fd - file descritpor for inotify
+ * @wfds - static vector for watchers fds
+ * @wd - watcher to remove
+ * @return - 0 on success, -1 on fail */
+int remove_watcher(int fd, struct static_vec *wfds, int wd)
+{
+	for(int i=0; i<wfds->len; ++i){
+		if(wfds->files[i].count == wd) {
+			fprintf(stderr, "Removing watcher on: %s\n", wfds->files[i].name);
+			int n = wfds->len;
+			inotify_rm_watch(fd, wd );
+			wfds->files[i].count = wfds->files[n-1].count;
+			strncpy(wfds->files[i].name, wfds->files[n-1].name, PATH_MAX);
+			wfds->len--;
+			return 0;
+		}
+	}
+	return -1;
+}
+
+/* Pause the watcher, it just removes the watchers but deos not alter the state
+ * of the data structure
+ * @fd - file descritpor for inotify
+ * @wfds - static vector for watchers fds
+ * @wd - watcher to suspend
+ * @return - 0 on success, -1 on fail */
+int pause_watcher(int fd, struct static_vec *wfds, int wd)
+{
+	for(int i=0; i<wfds->len; ++i){
+		if(wfds->files[i].count == wd) {
+			inotify_rm_watch(fd, wd );
+			return 0;
+		}
+	}
+	return -1;
+}
+
+/* Resume a suspended watcher, just create other wathcer on the place the old one
+ * was stored. Does not alter the state of the data structure
+ * @fd - file descritpor for inotify
+ * @wfds - static vector for watchers fds
+ * @wd - watcher to resume
+ * @return - new wd on success, -1 on fail */
+int resume_watcher(int fd, struct static_vec *wfds, int wd)
+{
+	for(int i=0; i<wfds->len; ++i){
+		if(wfds->files[i].count == wd) {
+			int wd = inotify_add_watch( fd, wfds->files[i].name, IN_ACCESS | IN_CREATE | IN_MODIFY);
+			wfds->files[i].count = wd;
+			return wd;
+		}
+	}
+	return -1;
+}
+
+char *get_wd_path(struct static_vec *wfds, int wd)
+{
+  for(int i=0; i<wfds->len; ++i) {
+    if(wfds->files[i].count == wd) {
+      return wfds->files[i].name;
+    }
+  }
+  return NULL;
+}
+
+int rm_watchers(int fd, struct static_vec *wfds)
+{
+	for(int i=0; i<wfds->len; ++i) {
+		inotify_rm_watch( fd, wfds->files[i].count);
+	}
+	return 0;
+}
+
 int main(int argc, char** argv)
 {
   int length, i = 0;
-  int fd, wd, wd2;
+  int fd;
   char buffer[EVENT_BUF_LEN];
   struct static_vec fls;
+  struct static_vec wfds;	// watchers
   static_vec_init(&fls);
+  static_vec_init(&wfds);
   signal(SIGINT, sgn_handl);
   if (argc < 2) {
     fprintf(stderr, "Please provide a directory to watch on\n"
@@ -149,8 +246,8 @@ int main(int argc, char** argv)
     fprintf(stderr, "No such directory: %s\n", argv[1]);
     exit(1);
   }
-  wd = inotify_add_watch( fd, argv[1], IN_ACCESS | IN_CREATE | IN_DELETE );
-  wd2 = inotify_add_watch( fd, "/home", IN_CREATE | IN_DELETE );
+
+  add_new_watcher(fd, &wfds, argv[1]);
 
   /*read to determine the event change happens on dir_name directory. Actually
    * this read blocks until the change event occurs*/ 
@@ -168,19 +265,31 @@ int main(int argc, char** argv)
     while ( i < length ) {     
       struct inotify_event *event = ( struct inotify_event * ) &buffer[ i ];
       if ( event->len ) {
-        count_file(event, &fls);
+			char *dir_path = get_wd_path(&wfds, event->wd);
+			char f_path[PATH_MAX];
+			snprintf(f_path, PATH_MAX-1, "%s/%s", dir_path, event->name);
+			// pause the watcher to don't count our own file read
+			pause_watcher(fd, &wfds, event->wd);
+			count_file(f_path, event, &fls);
+			// put back the watcher
+			resume_watcher(fd, &wfds, event->wd);
+			if ( (event->mask & IN_ISDIR) ) {
+				if (event->mask & IN_CREATE)
+					add_new_watcher(fd, &wfds, f_path);
+				if (event->mask & IN_DELETE) {
+					fprintf(stderr, "Got to remove wd: %d\n", event->wd);
+					remove_watcher(fd, &wfds, event->wd);
+				}
+
+			}
       }
       i += EVENT_SIZE + event->len;
     }
   }while(do_run);
-  /*removing the “/tmp” directory from the watch list.*/
-  inotify_rm_watch( fd, wd );
-  inotify_rm_watch( fd, wd2 );
 
   /*closing the INOTIFY instance*/
   close( fd );
 
   printf("The files:\n");
   print_files(&fls);
-
 }
